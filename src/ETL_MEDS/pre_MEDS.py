490ebbeb26f00d3d735c11a63d3857eb17698066
Matthew McDermott <mattmcdermott8@gmail.com>
2025-02-01 12:16:20 -0500

    Making pre-MEDS not a script by default.

--- src/ETL_MEDS/pre_MEDS.py
+++ src/ETL_MEDS/pre_MEDS.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-"""Performs pre-MEDS data wrangling for MIMIC-IV."""
+"""Performs pre-MEDS data wrangling for INSERT DATASET NAME HERE."""
 
 from pathlib import Path
 
@@ -8,4 +6,4 @@ from pathlib import Path
 def main(input_dir: Path, output_dir: Path, do_overwrite: bool | None = None):
     """Performs pre-MEDS data wrangling for INSERT DATASET NAME HERE."""
 
-    raise NotImplementedError("Please implement the pre_MEDS.py script for your dataset.")
+    raise NotImplementedError("Please write the pre_MEDS:main function for your dataset.")  # pragma: no cover
