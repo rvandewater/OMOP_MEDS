ddf3aeda85a99a50d4a3b13211400f8715c2b56c
Robin van de Water <r.p.vandewater@gmail.com>
2025-03-11 18:45:49 +0100

    create event_configs for not matching tables

--- src/OMOP_MEDS/__main__.py
+++ src/OMOP_MEDS/__main__.py
@@ -46,24 +46,45 @@ def main(cfg: DictConfig):
 
     # Step 1: Pre-MEDS Data Wrangling
     # if HAS_PRE_MEDS:
+    event_cfg = OmegaConf.load(EVENT_CFG)
+    # event_cfg.pop("subject_id_col")
+    # if cfg.get("tables_to_ignore", None):
+    event_cfg_new = event_cfg.copy()
     pre_MEDS_transform(cfg)
+    pre_MEDS_dir_files = [item.stem for item in pre_MEDS_dir.iterdir()]
+    for item in event_cfg.keys():
+        if item not in pre_MEDS_dir_files and item != "subject_id_col":
+            logger.warning(f"Removing table {item} from event config.")
+            event_cfg_new.pop(item)
+
+    if len(event_cfg_new.keys()) != len(event_cfg.keys()):
+        event_cfg_path = pre_MEDS_dir / "event_configs.yaml"
+        with open(event_cfg_path, "w") as f:
+            omegaconf.OmegaConf.save(config=event_cfg_new, f=f)
+    else:
+        event_cfg_path = EVENT_CFG
+        # EVENT_CFG = EVENT_CFG_TEMP
+    # ETL_CFG_TEMP = ETL_CFG+"_temp"
+    # with open(ETL_CFG_TEMP, "w") as f:
+    #     omegaconf.OmegaConf.save(config=event_cfg, f=f)
+
+
+
     # else:
     #     pre_MEDS_dir = raw_input_dir
-    event_cfg = OmegaConf.load(EVENT_CFG)
-    if cfg.get("tables_to_ignore", None):
-        event_cfg.tables_to_ignore = cfg.tables_to_ignore
-        with open(EVENT_CFG, "w") as f:
-            omegaconf.OmegaConf.save(config=event_cfg, f=f)
-    elif event_cfg.get("tables_to_ignore", None):
-        event_cfg.pop("tables_to_ignore")
-        with open(EVENT_CFG, "w") as f:
-            omegaconf.OmegaConf.save(config=event_cfg, f=f)
+
+    #     with open(EVENT_CFG, "w") as f:
+    #         omegaconf.OmegaConf.save(config=event_cfg, f=f)
+    # elif event_cfg.get("tables_to_ignore", None):
+    #     event_cfg.pop("tables_to_ignore")
+    #     with open(EVENT_CFG, "w") as f:
+    #         omegaconf.OmegaConf.save(config=event_cfg, f=f)
     # Step 2: MEDS Cohort Creation
     # First we need to set some environment variables
     command_parts = [
         f"DATASET_NAME={dataset_info.dataset_name}",
         f"DATASET_VERSION={dataset_info.raw_dataset_version}:{PKG_VERSION}:OMOP_{dataset_info.omop_version}",
-        f"EVENT_CONVERSION_CONFIG_FP={str(EVENT_CFG.resolve())}",
+        f"EVENT_CONVERSION_CONFIG_FP={str(event_cfg_path.resolve())}",
         f"PRE_MEDS_DIR={str(pre_MEDS_dir.resolve())}",
         f"MEDS_COHORT_DIR={str(MEDS_cohort_dir.resolve())}",
     ]
